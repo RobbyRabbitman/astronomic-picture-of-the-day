7a465c6c4443f59d98ecba54a6bca0791c8fbed3
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-11-22 21:33:09 +0100

    Added Apod model

---
+++ src/app/core/model/apod/Apod.ts
@@ -0,0 +1,10 @@
+export interface Apod {
+  title: string;
+  date: string;
+  explanation: string;
+  service_version: string;
+  media_type: string;
+  hdurl: string;
+  url: string;
+  copyright?: string;
+}
