458f3055048cdfdab279ab8a8aa398b103da6ea0
RobbyRabbitman <robby.rabbitman@gmail.com>
2020-11-22 21:29:38 +0100

    added core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from '@core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [CoreModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
