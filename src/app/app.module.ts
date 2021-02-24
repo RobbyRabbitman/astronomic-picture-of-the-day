0a47b9ccb437e6f6054f45e965aa01f055d1177f
RobbyRabbitman <robby.rabbitman@gmail.com>
2021-02-24 23:40:08 +0100

    PWA Support

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppComponent } from './app.component';
 import { CoreModule } from '@core/core.module';
 import { ShellModule } from '@features/shell/shell.module';
 import { AppRoutingModule } from '@routes';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, ShellModule, AppRoutingModule],
+  imports: [CoreModule, ShellModule, AppRoutingModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
